3a2c3729f8205475783b93ed38ad3139980fedf4
manuelbasanta <manuelbasanta1@gmail.com>
2023-06-15 16:19:37 -0300

    put back linter for builds

--- next.config.js
+++ next.config.js
@@ -12,12 +12,7 @@ const nextConfig = {
         sepoliaContractAddress: '0xEF22B89CF972aACbbBD3c48f577Ca1a107Bd8aed',
         hardhatContractAddress: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
         contractAddress: '0x4d0efa5fadd6D898436322b548fFE282AB0EfeBa'
-    },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+    }
 };
    
 module.exports = nextConfig;
