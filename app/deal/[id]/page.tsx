3016be29814def0579f51bf34a58157b561b7449
manuelbasanta <manuelbasanta1@gmail.com>
2023-06-20 00:14:24 -0300

    ico & Deal metadata

--- app/deal/[id]/page.tsx
+++ app/deal/[id]/page.tsx
@@ -1,4 +1,15 @@
+import { Metadata } from "next";
 import Deal from "../../../components/pages/Deal/Deal";
+
+export async function generateMetadata(
+    { params }
+  ): Promise<Metadata> {
+    const { id } = params;
+    return {
+      title: `Deal #${id} details`,
+      description: `Deal #${id} details and current state.`
+    }
+  }
 
 export default function Page({ params }) {
     const { id } = params;
